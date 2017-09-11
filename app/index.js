67e0714fa59a480f750ba4585a8af833d9ccdf59
tmwatchanan <tmwatchanan@gmail.com>
2017-09-12 00:41:21 +0700

    Add navbar and footer, using Bootstrap

--- app/index.js
+++ app/index.js
@@ -7,8 +7,19 @@ Vue.component('my-button', {
 })
 
 new Vue({
-  el: '#app',
+  el: '#my-button',
   data: {
-    message: "Hello There"
+    // message: "Hello Button"
+  }
+});
+
+Vue.component('my-navbar', {
+  template: '<nav class="navbar navbar-inverse">  <div class="container-fluid">    <div class="navbar-header">      <a class="navbar-brand" href="#">Fuzzy Web</a>    </div>    <ul class="nav navbar-nav navbar-right">      <li><a href="#">Learn</a></li>      <li><a href="#">Lab</a></li>      <li><a href="#">Assignment</a></li>      <li><a href="#">About Us</a></li>    </ul>  </div></nav>'
+})
+
+new Vue({
+  el: '#my-navbar',
+  data: {
+    // message: "Hello Navbar"
   }
 });
