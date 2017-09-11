ed18451b0d154c55e08c2ffb37f02ca3dd6c17a5
tmwatchanan <tmwatchanan@gmail.com>
2017-09-11 23:14:57 +0700

    Initiate Vue, Webpack, and Bootstrap

---
+++ app/index.js
@@ -0,0 +1,14 @@
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./css/main.css');
+import Vue from 'vue';
+
+Vue.component('my-button', {
+  template: '<input type="button" class="btn btn-primary" value="Hello   bootstrap">'
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    message: "Hello There"
+  }
+});
